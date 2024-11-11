ce1ba3fdd28c131dfbb98381c900fe307b2b453e
dincher.selimov <dincher.selimov@visma.com>
2024-11-11 17:02:33 +0200

    Add default images to the seeders and add Admin Panel button on Profile page

--- database/seeders/PizzaSeeder.php
+++ database/seeders/PizzaSeeder.php
@@ -13,36 +13,42 @@ class PizzaSeeder extends Seeder
             'name' => 'Margherita',
             'description' => 'Classic cheese and tomato Pizzas',
             'price' => 8.99,
+            'image' => 'images/FsZq235ep5qWFffTBbtgSzUPoDEGRDVZNkWryJSc.jpg',
         ]);
 
         Pizzas::create([
             'name' => 'Pepperoni',
             'description' => 'Topped with pepperoni slices',
             'price' => 9.99,
+            'image' => 'images/FsZq235ep5qWFffTBbtgSzUPoDEGRDVZNkWryJSc.jpg',
         ]);
 
         Pizzas::create([
             'name' => 'BBQ Chicken',
             'description' => 'Grilled chicken with BBQ sauce, onions, and cilantro',
             'price' => 10.99,
+            'image' => 'images/FsZq235ep5qWFffTBbtgSzUPoDEGRDVZNkWryJSc.jpg',
         ]);
 
         Pizzas::create([
             'name' => 'Vegetarian',
             'description' => 'Bell peppers, onions, mushrooms, and olives',
             'price' => 9.49,
+            'image' => 'images/FsZq235ep5qWFffTBbtgSzUPoDEGRDVZNkWryJSc.jpg',
         ]);
 
         Pizzas::create([
             'name' => 'Hawaiian',
             'description' => 'Ham and pineapple with mozzarella cheese',
             'price' => 10.49,
+            'image' => 'images/FsZq235ep5qWFffTBbtgSzUPoDEGRDVZNkWryJSc.jpg',
         ]);
 
         Pizzas::create([
             'name' => 'Four Cheese',
             'description' => 'A blend of mozzarella, cheddar, parmesan, and gorgonzola',
             'price' => 11.99,
+            'image' => 'images/FsZq235ep5qWFffTBbtgSzUPoDEGRDVZNkWryJSc.jpg',
         ]);
     }
 }
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,7 +4,15 @@
 
 <div class="container mx-auto py-12">
     <h2 class=" font-semibold text-3xl text-black leading-tight mb-6">Profile</h2>
-
+    @auth
+        @if(auth()->user()->role === 'admin')
+            <div>
+                <a href="/admin/food-items" class="inline-block bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300 mb-6 bg-opacity-80">
+                    Go to Admin Panel
+                </a>
+            </div>
+        @endif
+    @endauth
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
         <div class="bg-white dark:bg-gray-800 shadow rounded-lg p-6">
             @include('profile.partials.update-profile-information-form')
